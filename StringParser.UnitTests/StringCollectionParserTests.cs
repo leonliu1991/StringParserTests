2ef868dd349ddde23936138a99d9aaad96c59c7b
Xianhao Liu <xianhaoliu@XianhaoLiu-MacBook-Pro.local>
2024-04-02 21:32:55 +0100

    Add tests for StringParser

---
+++ StringParser.UnitTests/StringCollectionParserTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using Moq;
+using StringParser.Abstractions;
+using StringParser.Services;
+
+namespace StringParser.UnitTests;
+
+[TestFixture]
+public class StringCollectionParserTests
+{
+    private Mock<IStringParser> _stringParserMock = new Mock<IStringParser>();
+    private StringCollectionParser _collectionParser;
+
+    public StringCollectionParserTests()
+    {
+        _collectionParser = new StringCollectionParser(_stringParserMock.Object);
+    }
+
+    [Test]
+    public void PostiveTests_01_TestSingleObjectInTheCollection()
+    {
+        string testData = "123abc";
+        _stringParserMock.Setup(x => x.Parse(testData)).Returns(testData);
+
+        List<string> testInput = new List<string>();
+        testInput.Add(testData);
+
+        _collectionParser.Parse(testInput).ShouldBe(testInput);
+    }
+
+    [Test]
+    public void PostiveTests_02_TestMultipleObjectsInTheCollection()
+    {
+        string testData = "123abc";
+        _stringParserMock.Setup(x => x.Parse(testData)).Returns(testData);
+
+        List<string> testInput = new List<string>();
+        testInput.Add(testData);
+        testInput.Add(testData);
+
+        _collectionParser.Parse(testInput).ShouldBe(testInput);
+    }
+
+    [Test]
+    public void NegativeTests_01_TestEmptyResults()
+    {
+        string testData = "_4_4";
+        _stringParserMock.Setup(x => x.Parse(testData)).Returns(string.Empty);
+
+        List<string> testInput = new List<string>();
+        testInput.Add(testData);
+        testInput.Add(testData);
+
+        List<string> expectedOutput = new List<string>();
+        expectedOutput.Add("");
+        expectedOutput.Add("");
+
+        _collectionParser.Parse(testInput).ShouldBe(expectedOutput);
+    }
+
+    [Test]
+    public void NegativeTests_02_TestNullResults()
+    {
+        string testData = "";
+        _stringParserMock.Setup(x => x.Parse(testData)).Returns(value:null);
+
+        List<string> testInput = new List<string>();
+        testInput.Add(testData);
+        testInput.Add(testData);
+
+        List<string> expectedOutput = new List<string>();
+        expectedOutput.Add(null);
+        expectedOutput.Add(null);
+
+        _collectionParser.Parse(testInput).ShouldBe(expectedOutput);
+    }
+}
---
+++ StringParser.UnitTests/StringParserTests.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace StringParser.UnitTests;
+
+[TestFixture]
+public class StringParserTests
+{
+    private Services.StringParser parser = new Services.StringParser();
+
+    [Test]
+    public void PostiveTests_01_TestStandardOutput()
+    {
+        parser.Parse("AAAc91%cWwWkLq$1ci3_848v3d__K").ShouldBe("Ac91%cWwWkLq£1c");
+    }
+
+    [Test]
+    public void PostiveTests_02_TestMaximumLengthTruncation()
+    {
+        parser.Parse("12345678901234567890").ShouldBe("123567890123567");
+    }
+
+    [Test]
+    public void PostiveTests_03_TestDollarSignReplacement()
+    {
+        parser.Parse("$$$").ShouldBe("£");
+    }
+
+    [Test]
+    public void PostiveTests_04_TestCharacterDuplication()
+    {
+        parser.Parse("aaabbccAABBCC").ShouldBe("abcABC");
+    }
+
+    [Test]
+    public void PostiveTests_05_TestRemovalOfUnderscoreAndNumber4()
+    {
+        parser.Parse("a__4b_4_c_").ShouldBe("abc");
+    }
+
+    [Test]
+    public void PostiveTests_06_TestNonEnglishCharacters()
+    {
+        parser.Parse("ééé$$$ßß").ShouldBe("é£ß");
+    }
+
+    [Test]
+    public void PostiveTests_07_TestSpecialCharacters()
+    {
+        parser.Parse("1234567890!!!!!!").ShouldBe("123567890!");
+    }
+
+    [Test]
+    public void NegativeTests_01_TestEmptyInput()
+    {
+        parser.Parse(string.Empty).ShouldBe(null);
+    }
+
+    [Test]
+    public void NegativeTests_02_TestNullInput()
+    {
+        parser.Parse(null).ShouldBe(null);
+    }
+
+    [Test]
+    public void NegativeTests_03_TestStringOnlyHaveUnderscoresAndNumber4()
+    {
+        parser.Parse("_____444__4_").ShouldBe("");
+    }
+
+    [Test]
+    public void NegativeTests_04_TestStringsOfJust15Duplicates()
+    {
+        parser.Parse("aaaaaaaaaaaaaaa").ShouldBe("a");
+    }
+
+    [Test]
+    public void NegativeTests_05_TestAllExcludedCharacters()
+    {
+        parser.Parse("$_4_$$4__").ShouldBe("££");
+    }
+
+    [Test]
+    public void NegativeTests_06_TestVeryLongString()
+    {
+        parser.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").ShouldBe("a");
+    }
+}
